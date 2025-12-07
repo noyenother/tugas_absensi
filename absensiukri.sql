597c27b90d1e65f7949a8a22a96b77cf6b243b48
noyenother <journeynaya@gmail.com>
2025-12-08 02:42:44 +0700

    Add files via upload

---
+++ absensiukri.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 07, 2025 at 08:41 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbabsensi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `absensiukri`
+--
+
+CREATE TABLE `absensiukri` (
+  `id` int(11) NOT NULL,
+  `namamahasiswa` varchar(100) NOT NULL,
+  `npm` varchar(20) NOT NULL,
+  `kelas` varchar(10) NOT NULL,
+  `statuskehadiran` enum('Hadir','Sakit','Izin') NOT NULL,
+  `buktifoto` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `absensiukri`
+--
+
+INSERT INTO `absensiukri` (`id`, `namamahasiswa`, `npm`, `kelas`, `statuskehadiran`, `buktifoto`) VALUES
+(1, 'Khanaya Salsabila', '20241320021', 'A1', 'Hadir', '1765133734_20241320021.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `absensiukri`
+--
+ALTER TABLE `absensiukri`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `absensiukri`
+--
+ALTER TABLE `absensiukri`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
